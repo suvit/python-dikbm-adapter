c570ecd5965dae9e06452db1b3edc40e5144c521
Victor Safronovich <vsafronovich@gmail.com>
2013-01-23 08:40:51 +0500

    try to connect to rsa server after file lock

--- dikbm_adapter/main.py
+++ dikbm_adapter/main.py
@@ -11,28 +11,30 @@ logger = logging.getLogger(__name__)
 
 def main():
     logger.info('Starting DiKBM python client')
+
+    lock = FileLock("dikbm")
     try:
-        client = DiKBMClient()
-    except:
-        logger.exception('Connect Error')
+        lock.acquire(0)
+    except AlreadyLocked:
+        logger.info('lock %s already locked' % lock.unique_name)
+    except LockFailed:
+        logger.error('lock %s cant be locked' % lock.unique_name)
     else:
-        lock = FileLock("dikbm")
+        logger.debug('lock %s acquired' % lock.unique_name)
+
         try:
-            lock.acquire(0)
-        except AlreadyLocked:
-            logger.info('lock %s already locked' % lock.unique_name)
-        except LockFailed:
-            logger.error('lock %s cant be locked' % lock.unique_name)
+            client = DiKBMClient()
+        except:
+            logger.exception('Connect Error')
         else:
-            logger.debug('lock %s acquired' % lock.unique_name)
             try:
                 client.proceed_in()
                 client.proceed_status()
             except:
                 logger.exception('Proceed Error')
-            finally:
-                lock.release()
-                logger.debug('lock %s released' % lock.unique_name)
+        finally:
+            lock.release()
+            logger.debug('lock %s released' % lock.unique_name)
     finally:
         logger.info('Finished DiKBM python client')
 
